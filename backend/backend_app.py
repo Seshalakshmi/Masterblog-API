9156675a7bc821c81746ddec3e7d8d4e55524c02
sesha <sesha@yahoo.com>
2026-05-29 12:53:11 +0200

    Added error handling for add and delete post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -10,8 +10,10 @@ POSTS = [
 ]
 
 def validate_book_data(data):
-    if "title" not in data or "content" not in data:
-        return False
+    if "title" not in data:
+        return "title is missing"
+    elif "content" not in data:
+        return "content is missing"
     return True
 
 @app.route('/api/posts', methods=['GET', 'POST'])
@@ -25,12 +27,12 @@ def get_and_add_posts():
         new_post = request.get_json()
 
         if not validate_book_data(new_post):
-            return jsonify({"error": "Invalid post data"}), 400
+            return jsonify({f"Message: {new_post}"}), 400
 
         new_post['id'] = post_id
         POSTS.append(new_post)
         return jsonify(new_post), 201
-    return jsonify(POSTS)
+    return jsonify(POSTS), 200
 
 
 def find_post_by_id(post_id):
@@ -40,12 +42,30 @@ def find_post_by_id(post_id):
     return None
 
 
-@app.route('/api/posts/<id>', methods=["DELETE"])
-def delete_post(post_id):
-    deleted_post = find_post_by_id(post_id)
+@app.route('/api/posts/<int:id>', methods=["DELETE"])
+def delete_post(id):
+    deleted_post = find_post_by_id(id)
+    if not deleted_post:
+        return jsonify({f"Message: Post {id} is not found"}), 404
     POSTS.remove(deleted_post)
 
-    return jsonify(POSTS)
+    return jsonify(
+        {f"message: Post with id {id} has been deleted successfully."}), 200
+
+
+@app.errorhandler(400)
+def not_found_error(error):
+    return jsonify({"error": "Invalid post data"}), 400
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({"error": "Not Found"}), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return jsonify({"error": "Method Not Allowed"}), 405
 
 
 if __name__ == '__main__':
